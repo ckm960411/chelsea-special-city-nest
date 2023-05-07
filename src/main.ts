9f30d21221e9a438159576d2c1312854e9106e53
경민 <a01098956117@gmail.com>
2023-05-07 22:34:54 +0900

    mani.ts 에 swagger 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get<ConfigService>(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('C.I.C')
+    .setDescription('chelsea-special-city')
+    .setVersion('1.0.0')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); // Swagger API 에 엔드포인트를 지정해줌
 
   const PORT = configService.get<string>('PORT');
 
