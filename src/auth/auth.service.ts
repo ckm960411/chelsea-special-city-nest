f3d76a08a102c2a37ff0bddb609982ee5a821f21
경민 <a01098956117@gmail.com>
2023-05-21 21:39:13 +0900

    회원가입시 DB 저장

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {}
+
   signUp(authCredentialsDto: AuthCredentialsDto) {
-    return authCredentialsDto;
+    return this.userRepository.registerUser(authCredentialsDto);
   }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,35 @@
 import { CustomRepository } from 'src/typeorm/typeorm-ex.decarator';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { hash } from 'bcrypt';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async registerUser(authCredentialsDto: AuthCredentialsDto) {
+    const { username, email, password } = authCredentialsDto;
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = this.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('이미 존재하는 이메일입니다');
+      } else {
+        console.log(error);
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
