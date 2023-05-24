d5fb5f5946601462aa469184cbcf799eb5b0444f
ckm960411 <a01098956117@gmail.com>
2023-05-24 22:31:16 +0900

    aws-sdk 구성

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { S3 } from 'aws-sdk';
 
 @Injectable()
-export class AwsService {}
+export class AwsService {
+  private readonly bucketName: string;
+  private readonly s3: S3;
+
+  constructor(private configService: ConfigService) {
+    this.bucketName = this.configService.get<string>('AWS_BUCKET');
+    this.s3 = new S3({
+      region: this.configService.get<string>('AWS_REGION'),
+      accessKeyId: this.configService.get('AWS_ACCESS_KEY'),
+      secretAccessKey: this.configService.get('AWS_SECRET_KEY'),
+    });
+  }
+}
